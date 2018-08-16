2dd84137a50212fdffe9f40df6f9f89c2163c58b
Sairam Venkata Krishna Maddula <vksmaddula@virtusa.com>
2018-08-16 17:45:33 +0530

    added entry through redux

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import * as contactAction from './actions/contactAction';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    }
+    this.fieldChange = this.fieldChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+  }
+
+  fieldChange(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  formSubmit(event) {
+
+    event.preventDefault();
+    let contact = {
+      name: this.state.name
+    }
+    this.props.createContact(contact);
+
+
+    console.log(this.state.name)
+
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <h4>Phone Book In Redux And React</h4>
+        <hr />
+        {<ul>
+          {this.props.contacts.map((contact, i) => <li key={i}>{contact.name}</li>)}
+        </ul>}
+
+        <div className="phonebook">
+          <h5>Create a New Contact</h5>
+          <form onSubmit={this.formSubmit}>
+            <input type="text" onChange={this.fieldChange} />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    contacts: state.contacts
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createContact: contact => dispatch(contactAction.createContact(contact))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/actions/contactAction.js
@@ -0,0 +1,9 @@
+import * as actionTypes from '../actions/actionTypes';
+
+export const createContact  = (contact) => {
+    return {
+        type:actionTypes.CREATE_NEW_CONTACT,
+        contact:contact
+    }
+    
+}
